d33252886815d9018dedc7d7777687d3d9c96b01
BeaverNotACat <beavernotacat@gmail.com>
2024-07-26 01:16:19 +1000

    ✨ Partly added databhase gateway

--- app/adapters/database/connection.py
+++ app/adapters/database/connection.py
@@ -1,7 +1,13 @@
+from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
+from app.adapters.database.models import FormPopulationODM
 from app.settings import settings
 
 
-def mongodb_session_factory():
-    return AsyncIOMotorClient(settings.mongodb_url)
+async def mongodb_session_factory():
+    client = AsyncIOMotorClient(settings.mongodb_url)
+    await init_beanie(
+        database=client.get_database(settings.MONGO_DATABASE),
+        document_models=[FormPopulationODM]
+    )
--- app/adapters/database/repository.py
+++ app/adapters/database/repository.py
@@ -1,13 +1,23 @@
+from app.adapters.database.connection import mongodb_session_factory
 from app.application.gateway import FormPopulationReader, FormPopulationSaver
 from app.domain.models import UserEmail, FormId, FormPopulation
 from app.adapters.database.models import FormPopulationODM
 
 class FormPopulationGateway(FormPopulationReader, FormPopulationSaver):
-    def get_filtered_form_populations_list(
+    model = FormPopulationODM
+    session_factory = mongodb_session_factory
+
+    async def __init__(self) -> None:
+        await self.session_factory()
+
+    async def get_filtered_form_populations_list(
         self, user_email: UserEmail, form_id: FormId
     ) -> list[FormPopulation]:
-        FormPopulation.find(FormPopulation.user_email == user_email).find(FormPopulation.form_id == form_id).to_list()
-        
-        return FormPopulation()
+        query = FormPopulationODM.find(FormPopulationODM.user_email == user_email).find(FormPopulationODM.form_id == form_id)
+        result = await query.to_list()
+        return [FormPopulation(**population) for population in result] # type: ignore
 
-    
+    async def bulk_save_form_populations(
+        self, populations: list[FormPopulation]
+    ) -> list[FormPopulation]:
+        raise NotImplementedError    
