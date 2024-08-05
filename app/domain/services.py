f841752cae28dd0a8a1893bb04ae974d6754902e
BeaverNotACat <beavernotacat@gmail.com>
2024-08-05 19:33:56 +0000

    ✨ Add url creation

--- app/domain/services.py
+++ app/domain/services.py
@@ -1,5 +1,21 @@
 from .models import FormPopulation, Parameter, User
 
+class ParameterService:
+    @staticmethod
+    def create(data: dict) -> Parameter:
+        return Parameter(**data)
+
+    @staticmethod
+    def to_url_parameters(data: list[Parameter]) -> str:
+        res = ""
+        for item in data:
+            res += f"&{item.field}={item.answer}"
+        return res[1:]
+
+    @staticmethod
+    def create_from_dict(data: dict[str, str]) -> list[Parameter]:
+        return [Parameter(field=field, answer=answer) for field, answer in data.items()]
+
 
 # TODO refactor
 class FormPopulationService:
@@ -15,16 +31,9 @@ class FormPopulationService:
     def add_parameter(form_population: FormPopulation, parameter: Parameter) -> None:
         form_population.parameters.append(parameter)
 
-
-class ParameterService:
     @staticmethod
-    def create(data: dict) -> Parameter:
-        return Parameter(**data)
-
-    @staticmethod
-    def create_from_dict(data: dict[str, str]) -> list[Parameter]:
-        return [Parameter(field=field, answer=answer) for field, answer in data.items()]
-
+    def to_link_path(data: FormPopulation) -> str:
+        return f"/{data.form_id}?{ParameterService.to_url_parameters(data.parameters)}"
 
 class UserService:
     @staticmethod
