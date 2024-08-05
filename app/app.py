7fd125492c930d55d76e95f9e0b3c2495b0035cf
BeaverNotACat <beavernotacat@gmail.com>
2024-08-05 18:46:30 +0000

    ✨ Add jinja templating

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
+from pathlib import Path
+
 from litestar import Litestar
 from litestar.config.cors import CORSConfig
+from litestar.contrib.jinja import JinjaTemplateEngine
+from litestar.template import TemplateConfig
 
 cors_config = CORSConfig(allow_origins=["*"])
 app = Litestar(
     cors_config=cors_config,
+    template_config=TemplateConfig(
+        directory=Path("templates"),
+        engine=JinjaTemplateEngine,
+    )
 )
