703e0437e68d450949894e095f2007f364614f7c
BeaverNotACat <beavernotacat@gmail.com>
2024-08-23 14:24:15 +1000

    WIP 🚧 Add forms API request

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import {
   BrowserRouter as Router,
 } from "react-router-dom";
 
-import './App.css';
 import Login from './routes/Login'
 import Forms from './routes/Forms'
 
--- frontend/src/routes/Forms.jsx
+++ frontend/src/routes/Forms.jsx
@@ -13,33 +13,52 @@ const Forms = () => {
         var response = await fetch(settings.API_URI);
         if (!response.ok) {
           throw new Error('Responce is not 200 ok')
-        };   
+        };
+        console.log(response);
         setFormLinks(await response.json());
-      } catch (err){
+      } catch (err) {
         console.error(err)
+        setFormLinks([]);
       }
-    })()});
+    })()
+  });
 
   return (
-    <ul>
-      {formLinks.map((formPopulation) => {
-        <li>
-          <a href="">
-            {assembleLink(formPopulation)}
-          </a>
-        </li>
-      })}
-    </ul>
+    <div>
+      <h1>
+        Анкты для проходения:
+      </h1>
+      <ul>
+        {formLinks.map((formPopulation) => {
+          return (
+            <li>
+              <a href={assembleLink(formPopulation)}>
+                {assembleTitle(formPopulation)}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   );
 }
 
 
 function assembleLink(formPopulation) {
-  const link = `${settings.YANDEX_FORMS_URI}/${formPopulation.form_id}?`;
+  var link = `${settings.YANDEX_FORMS_URI}/${formPopulation.form_id}?`;
   for (let parameter in formPopulation.parameters) {
     link += `${parameter.field}=${parameter.answer}&`;
   }
-  return link.slice(0, -1)
+  return link.slice(0, -1);
+}
+
+
+function assembleTitle(formPopulation) {
+  var title = '';
+  for (let parameter in formPopulation.parameters) {
+    title += `${parameter.field} = ${parameter.answer}\n`;
+  }
+  return title.slice(0, -1)
 }
 
 export default Forms;
